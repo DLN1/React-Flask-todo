fc2c89e168dc8496d4bfd37b16995c2f76555281
DLN1 <jacobcarlberg@outlook.com>
2022-05-11 11:15:01 +0200

    rename

--- ui/src/services/backend.js
+++ ui/src/services/backend.js
@@ -2,16 +2,16 @@ import axios from "axios";
 
 export default {
   get_todos() {
-    return axios.get("http://localhost:5000/todos");
+    return axios.get("https://todo-react-flask-app.herokuapp.com/todos");
   },
   create_todo(data) {
-    return axios.post("http://localhost:5000/todos", data);
+    return axios.post("https://todo-react-flask-app.herokuapp.com/todos", data);
   },
   update_todo(id, item) {
-    return axios.put(`http://localhost:5000/todos/${id}`, item);
+    return axios.put(`https://todo-react-flask-app.herokuapp.com/todos/${id}`, item);
   },
   remove_todos(id) {
     console.log(id)
-  return axios.delete(`http://localhost:5000/todos`);
+  return axios.delete(`https://todo-react-flask-app.herokuapp.com/todos`);
 },
 };
